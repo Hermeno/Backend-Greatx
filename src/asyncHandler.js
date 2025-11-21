611a329e9cb5f202c660a563b605b7b4fe85ba9a
Herminio dev <86198523+Hermeno@users.noreply.github.com>
2025-11-21 08:10:08 +0200

    Add asyncHandler

--- src/asyncHandler.js
+++ src/asyncHandler.js
@@ -1,4 +1,10 @@
-// src/middleware/asyncHandler.js
-module.exports = fn => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+module.exports = function asyncHandler(fn) {
+  if (typeof fn !== 'function') {
+    console.error('asyncHandler ERROR: argumento não é função:', fn);
+    throw new TypeError('asyncHandler: argumento não é uma função');
+  }
+
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
 };
